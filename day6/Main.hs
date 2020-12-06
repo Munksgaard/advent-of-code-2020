65106c05c158e625487ac9cf473557d22c5d8ed8
Philip Munksgaard <philip@munksgaard.me>
2020-12-06 11:02:50 +0100

    Even more nice

--- day6/Main.hs
+++ day6/Main.hs
@@ -3,19 +3,12 @@ module Main where
 import Data.Char
 import Data.Function
 import Data.List
-
-paragraphs :: String -> [String]
-paragraphs [] = []
-paragraphs s =
-  let (para, rest) = helper [] s
-   in para : paragraphs rest
-  where
-    helper acc [] = (reverse acc, [])
-    helper acc ('\n' : '\n' : rest) = (reverse acc, dropWhile isSpace rest)
-    helper acc (c : rest) = helper (c : acc) rest
+import Data.List.Split
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 main :: IO ()
 main = do
-  input <- paragraphs <$> getContents
+  input <- splitOn "\n\n" <$> getContents
   print $ sum $ fmap (length . nub . filter isAlpha) input
   print $ sum $ fmap (length . foldr intersect ['a' .. 'z'] . words) input
